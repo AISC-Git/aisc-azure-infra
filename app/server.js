0baa486ce43b55eed2e0aa9c9ea99fca02d4f97e
Gopal <gopalk@aiscworld.com>
2026-04-14 11:31:16 -0400

    Trigger redeploy v2

--- app/server.js
+++ app/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Dev environment running 🚀");
-});
+	res.send("Dev environment running v2 🚀");});
 
 app.listen(process.env.PORT || 3000);
