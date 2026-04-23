fbffe1a274a3f4d8409a62866da77fe7ae04ba42
AISC <solarenergykhar@gmail.com>
2026-04-22 22:32:23 -0400

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  output: 'export', //enables static export
+  trailingSlash: true, 
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
